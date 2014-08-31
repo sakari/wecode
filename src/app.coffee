5749b7fefd9fa53bfbf74a4a099bfce794d605a9
sakari jokinen <sakariij@gmail.com>
2014-08-31 13:26:46 +0300

    use require-cs & coffee-script

---
+++ src/app.coffee
@@ -0,0 +1,2 @@
+define (r) ->
+    alert 'gogo-gorilla'
--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 require.config({
-    baseUrl: '/'
+    baseUrl: '/',
+    paths: {
+        'coffee-script': 'lib/coffee-script',
+        'cs': 'lib/cs'
+    }
 });
+
+require(['cs!src/app'], function() {
+})
